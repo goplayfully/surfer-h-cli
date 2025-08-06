f1ec9e62f71cac73e5479f19a6f9cb8affa4ee95
Mark Risher <218038+mrisher@users.noreply.github.com>
2025-08-06 14:21:52 -0700

    update branding

---
+++ frontend/src/components/common/icons/OutgoingLogomark.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const OutgoingLogomark = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg viewBox="0 0 13 13" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <path d="M12.0631 6.03155C12.0631 9.36269 9.36269 12.0631 6.03155 12.0631C2.70042 12.0631 0 9.36269 0 6.03155C0 2.70042 2.70042 0 6.03155 0C9.36269 0 12.0631 2.70042 12.0631 6.03155Z" fill="#3A39FF"/>
+    <path d="M0 6.03155H12.0631V10.1602C12.0631 11.2111 11.2111 12.0631 10.1602 12.0631H1.90291C0.851963 12.0631 0 11.2111 0 10.1602V6.03155Z" fill="#3A39FF"/>
+  </svg>
+);
+
+export default OutgoingLogomark;
